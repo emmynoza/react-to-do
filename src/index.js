76ddb468ad19fa3f5d74fd1aed7b0f264d3a7592
Emerson Espinoza <espinozer93@gmail.com>
2022-02-17 13:07:01 -0500

    init commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className='app'>
+                <h1>todos</h1>
+                <input className='input' id='input' placeholder='What needs to be done?' onClick={createTask}>
+                </input>
+
+            </div >
+        )
+    }
+}
+
+function createTask() {
+    console.log('I was clicked');
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App />,
+    document.getElementById('root')
+)
